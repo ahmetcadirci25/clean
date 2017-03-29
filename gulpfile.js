6d26127e61f06207e72978bba6b269fb4de726cd
Ahmet CADIRCI <ahmetcadirci25@gmail.com>
2017-03-29 21:04:10 +0300

    1.0.1 gulp eklendi. resim ve kod düzenlemesi yapıldı.

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+
+gulp.task('sass', function () {
+	gulp.src('scss/main.scss')
+		.pipe(sass({includePaths: ['scss']}))
+		.pipe(gulp.dest('css'));
+});
+
+gulp.task('browser-sync', function () {
+	browserSync.init(["css/*.css", "js/*.js"], {
+		server: {
+			baseDir:"./"
+		}
+	});
+});
+
+gulp.task('default', ['sass', 'browser-sync'], function(){
+	gulp.watch("scss/*.scss", ['sass']);
+	gulp.watch('*.html', browserSync.reload); 
+});
+
